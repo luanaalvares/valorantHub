064ff704f9d1a4d19ced38a6a19c13827e519467
Thiago Arcoverde <tluiz2701@gmail.com>
2024-06-23 16:24:16 -0300

    fix auth guard module error

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,19 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { UserModule } from 'src/user/user.module';
 import { JwtModule } from '@nestjs/jwt';
+import { AuthController } from './auth.controller';
 import { jwtConstants } from './constants';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [UserModule,
+  imports: [
+    UserModule,
     JwtModule.register({
-        global: true,
-        secret: jwtConstants.secret,
-        signOptions: { expiresIn: '60s' },
-      }),
-    ],
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '6000s' },
+    }),
+  ],
   providers: [AuthService],
   controllers: [AuthController],
+  exports: [AuthService],
 })
 export class AuthModule {}
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -3,12 +3,19 @@ import { UserService } from './user.service';
 import { UserController } from './user.controller';
 import { User, UserSchema } from './schema/user.schema';
 import { MongooseModule } from '@nestjs/mongoose';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstants } from 'src/auth/constants';
 
 @Module({
   controllers: [UserController],
   providers: [UserService],
   imports: [
+    JwtModule.register({
+      global: true,
+      secret: jwtConstants.secret,
+      signOptions: { expiresIn: '6000s' },
+    }),
     MongooseModule.forFeature([{ name:User.name, schema: UserSchema }])
-  ]
+  ],
 })
 export class UserModule {}
